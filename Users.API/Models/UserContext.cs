c30cc1f7f1e5af8630c73d19eece1a50f054bfe5
Tommy Berglund <tompab84@hotmail.com>
2019-10-18 15:24:12 +1000

    early build - testing docker

---
+++ Users.API/Models/UserContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Users.API.Models {
+    public class UserContext : DbContext {
+
+        public UserContext(DbContextOptions<UserContext> options) : base(options) {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
