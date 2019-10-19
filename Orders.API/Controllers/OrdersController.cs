97a4623895cf4ab30815c1501ee15821b5c5aaaa
Tommy Berglund <tompab84@hotmail.com>
2019-10-19 17:04:46 +1000

    added the epi endpoints

--- Orders.API/Controllers/OrdersController.cs
+++ Orders.API/Controllers/OrdersController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -7,7 +6,7 @@ using Orders.API.Models;
 
 namespace Orders.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("store/order")]
     [ApiController]
     public class OrdersController : ControllerBase
     {
@@ -18,14 +17,14 @@ namespace Orders.API.Controllers
             _context = context;
         }
 
-        // GET: api/Orders
+        // GET: store/orders
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
-        {
+        [Route("~/store/orders")]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders() {
             return await _context.Orders.ToListAsync();
         }
 
-        // GET: api/Orders/5
+        // GET: store/order/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Order>> GetOrder(long id)
         {
@@ -39,37 +38,7 @@ namespace Orders.API.Controllers
             return order;
         }
 
-        // PUT: api/Orders/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutOrder(long id, Order order)
-        {
-            if (id != order.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(order).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!OrderExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Orders
+        // POST: store/order
         [HttpPost]
         public async Task<ActionResult<Order>> PostOrder(Order order)
         {
@@ -79,7 +48,7 @@ namespace Orders.API.Controllers
             return CreatedAtAction(nameof(GetOrder), new { id = order.Id }, order);
         }
 
-        // DELETE: api/Orders/5
+        // DELETE: store/order/5
         [HttpDelete("{id}")]
         public async Task<ActionResult<Order>> DeleteOrder(long id)
         {
@@ -93,11 +62,6 @@ namespace Orders.API.Controllers
             await _context.SaveChangesAsync();
 
             return order;
-        }
-
-        private bool OrderExists(long id)
-        {
-            return _context.Orders.Any(e => e.Id == id);
         }
     }
 }
